0468b200b567c81bcd5544c8e5e0a6c5ede3eece
jim.wang <jim.wang@skymizer.com>
2020-07-14 06:30:14 +0000

    Initialization

---
+++ x.py
@@ -0,0 +1,11 @@
+import time                                                                                                                                                                               
+  
+def messager():
+    print("Echo some message.")
+ 
+if __name__ == "__main__":
+    print("This is x.py")
+    print("Loading......")
+    time.sleep(100)
+    messager()
+
---
+++ y.py
@@ -0,0 +1,6 @@
+from x import messager                                                                                                                                                                    
+from trains import Task
+task = Task.init(project_name='Example', task_name='Import test')
+  
+if __name__ == "__main__":
+    messager()
